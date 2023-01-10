de36af5fab74b7b2417efc8d9be7cd05042620cd
Pankaj <you@example.com>
2023-01-10 20:31:41 +0530

    sum of two num

---
+++ SumofTwoNum.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class SumofTwoNum{
+
+	public static int Sum(int a, int b){
+		int x = a;
+		int y = b;
+		return x+y;
+	}
+
+	public static void main(String[] args) {
+		int a, b;
+		Scanner sc=new Scanner(System.in);
+		a=sc.nextInt();
+		b=sc.nextInt();
+		System.out.println(Sum(a, b));
+	}
+}
