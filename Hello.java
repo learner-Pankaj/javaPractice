7ad9ac0a9aab9e9503284adfcb03a2bee9726b06
Pankaj <ydvpnkj28@gmail.com>
2025-03-12 12:44:06 +0530

    Day1, Simple program Practice

---
+++ Hello.java
@@ -0,0 +1,8 @@
+package practice.javaPractice;
+
+class Hello{
+    public static void main(String []args){
+        System.out.println("This is my first program, Thankyou!!");
+    }
+}
+
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # javaPractice
 This is practice Program, I'll be updating on daily basis.
+
+Day 1
+    Today, I'll be practicing simple basic programs.
